d68ca78fb9ffd4668b3cba854cb6c33c5a20d407
Yannick <yannkhe@gmail.com>
2023-10-15 08:20:40 +0200

    add unit tests

---
+++ Makefile
@@ -0,0 +1,9 @@
+SHELL := /bin/bash
+
+tests:
+	APP_ENV=test symfony console doctrine:database:drop --force || true
+	APP_ENV=test symfony console doctrine:database:create
+	APP_ENV=test symfony console doctrine:migrations:migrate -n
+	APP_ENV=test symfony console doctrine:fixtures:load -n
+	symfony php bin/phpunit $(MAKECMDGOALS)
+.PHONY: tests
--- migrations/Version20231014194410.php
+++ migrations/Version20231014194410.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230930053254 extends AbstractMigration
+final class Version20231014194410 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,12 +21,13 @@ final class Version20230930053254 extends AbstractMigration
     {
         $this->addSql('
             CREATE TABLE sessions (
-                        sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
-                        sess_data BYTEA NOT NULL,
-                        sess_lifetime INTEGER NOT NULL,
-                        sess_time INTEGER NOT NULL
-                    )
+                sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
+                sess_data BYTEA NOT NULL,
+                sess_lifetime INTEGER NOT NULL,
+                sess_time INTEGER NOT NULL
+            )
         ');
+        
         $this->addSql('CREATE INDEX expiry ON sessions (sess_lifetime)');
 
     }
